bd84f8567aaf4c9bc5038f432eae058034011d06
P-Pranjali <pranjali.palsodkar@gmail.com>
2026-06-11 09:15:52 +0530

    Add 3958-minimum-removals-to-balance-array.java

---
+++ 3958-minimum-removals-to-balance-array/3958-minimum-removals-to-balance-array.java
@@ -0,0 +1,20 @@
+class Solution {
+  
+           public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);  
+        int n = nums.length;
+        int maxLen = 0;
+        int i = 0;  
+
+        for (int j = 0; j < n; j++) { 
+            
+            while (nums[j] > (long) k * nums[i]) {  
+                i++;
+            }
+            maxLen = Math.max(maxLen, j - i + 1);
+        }
+
+        return n - maxLen; 
+    }
+    
+}
