6bcea54f9f4480b2697f36d5f1dc1000b8c14525
P-Pranjali <pranjali.palsodkar@gmail.com>
2026-07-11 10:51:18 +0530

    Add 0242-valid-anagram.java

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()  != t.length()) return false;
+
+        HashMap<Character, Integer> map  = new HashMap<>();
+
+       for(int i = 0; i < s.length(); i++){
+           map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+       }
+
+        for(int j = 0; j < t.length() ; j++){
+
+        char c = t.charAt(j);
+
+        if(!map.containsKey(c) || map.get(c) == 0){
+            return false;
+        }
+
+        map.put(c, map.get(c)-1);
+        }
+       
+        return true;
+    }
+}
