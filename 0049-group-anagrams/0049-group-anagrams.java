0846be26029708087ce36bd852ce6a8f14023178
P-Pranjali <pranjali.palsodkar@gmail.com>
2026-07-12 11:20:45 +0530

    Update 0049-group-anagrams.java

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,26 +1,38 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-      HashMap<String, List<String>> map = new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
 
+            char[] ch = s.toCharArray();
 
-      for(String str : strs){
+            Arrays.sort(ch);
 
-        int[] count = new int[26];
+        String key = new String(ch);
 
-        for(char c : str.toCharArray()){
-
-            count[c-'a']++;
+            map.computeIfAbsent(key, k-> new ArrayList<>()).add(s);
         }
 
-        String key = Arrays.toString(count);
-
-        map.computeIfAbsent(key, k-> new ArrayList<>());
-            map.get(key).add(str);
-
-      }
-
-            return new ArrayList<>(map.values());
-        
+        return new ArrayList<>(map.values());
     }
 }
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+    //   HashMap<String, List<String>> map = new HashMap<>();
+    //   for(String str : strs){
+    //     int[] count = new int[26];
+    //     for(char c : str.toCharArray()){
+
+    //         count[c-'a']++;
+    //     }
+
+    //     String key = Arrays.toString(count);
+
+    //     map.computeIfAbsent(key, k-> new ArrayList<>());
+    //         map.get(key).add(str);
+
+    //   }
+    //         return new ArrayList<>(map.values());
+    //}
+    //}
