3eba5c15b64586d0bb97728878560b157b8fac2f
P-Pranjali <pranjali.palsodkar@gmail.com>
2026-07-16 12:59:21 +0530

    Update 0155-min-stack.java

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,9 +1,46 @@
+// class MinStack {
+
+//      Deque<Integer> stackA;
+//     Deque<Integer> stackB;
+
+//     //int currentMin = Integer.MAX_VALUE;
+
+//     public MinStack() {
+//         stackA = new ArrayDeque<>();
+//         stackB = new ArrayDeque<>();
+        
+//     }
+    
+//     public void push(int value) {
+//         stackA.push(value);
+//         if(stackB.isEmpty()) stackB.push(value);
+//       else{int currentMin = Math.min(value, stackB.peek());
+//         stackB.push(currentMin);}
+        
+//     }
+    
+//     public void pop() {
+//         stackA.pop();
+//         stackB.pop();
+        
+//     }
+    
+//     public int top() {
+//        return stackA.peek();
+        
+//     }
+    
+//     public int getMin() {
+//         return stackB.peek();
+        
+//     }
+// }
+
+
 class MinStack {
 
      Deque<Integer> stackA;
     Deque<Integer> stackB;
-
-    //int currentMin = Integer.MAX_VALUE;
 
     public MinStack() {
         stackA = new ArrayDeque<>();
@@ -13,15 +50,19 @@ class MinStack {
     
     public void push(int value) {
         stackA.push(value);
+
         if(stackB.isEmpty()) stackB.push(value);
-      else{int currentMin = Math.min(value, stackB.peek());
-        stackB.push(currentMin);}
+
+      else if (value <= stackB.peek()){   
+        stackB.push(value);}
         
     }
     
     public void pop() {
+           if(stackA.peek().equals(stackB.peek()))
+            stackB.pop();
         stackA.pop();
-        stackB.pop();
+     
         
     }
     
