f185dae7e1d53fc592d007834b8e8a8b41c77a12
P-Pranjali <pranjali.palsodkar@gmail.com>
2026-07-24 16:34:16 +0530

    Update 0217-contains-duplicate.java

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,14 +3,31 @@ class Solution {
 
         HashSet<Integer> set = new HashSet<>();
 
-        for(int i = 0; i < nums.length; i++){
+        for(int i=0; i< nums.length; i++){
 
-            if(set.contains(nums[i])){
-
-                return true;
+            if(set.contains(nums[i])){  //checkin if current nums[i] value is present in hashMap 
+                return true;       //If present return true
             }
-            set.add(nums[i]);
+
+            set.add(nums[i]);  //putting value and index in hashMap.
         }
         return false;
     }
 }
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int i=0; i< nums.length; i++){
+
+//             if(map.containsKey(nums[i])){  //checkin if current nums[i] value is present in hashMap 
+//                 return true;       //If present return true
+//             }
+
+//             map.put(nums[i], i);  //putting value and index in hashMap.
+//         }
+//         return false;
+//     }
+// }
