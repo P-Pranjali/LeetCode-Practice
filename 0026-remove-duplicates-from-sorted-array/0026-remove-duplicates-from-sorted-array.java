101638290890a15c998a9469460903d2cf62626c
P-Pranjali <pranjali.palsodkar@gmail.com>
2026-07-25 12:25:17 +0530

    Add 0026-remove-duplicates-from-sorted-array.java

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+       
+       if(nums.length == 0) return 0;
+
+       int j = 1;
+       for(int i = 1; i < nums.length; i++){
+        if(nums[i] != nums[i-1]){
+            nums[j] = nums[i];
+            j++;
+        }
+       }
+        return j;
+    }
+}
+
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+       
+//        if(nums.length == 0) return 0;
+
+//        int i = 0;
+//        for(int j = 1; j < nums.length; j++){
+//         if(nums[j] != nums[i]){
+//             i++;
+//             nums[i] = nums[j];
+//         }
+//        }
+//         return i+1;
+//     }
+// }
