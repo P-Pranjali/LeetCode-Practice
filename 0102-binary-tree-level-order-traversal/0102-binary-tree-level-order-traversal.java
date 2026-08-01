34e60d5b263894c8543ebe31ff5f1a8545d3f9dd
P-Pranjali <pranjali.palsodkar@gmail.com>
2026-08-01 07:57:39 +0530

    Update 0102-binary-tree-level-order-traversal.java

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,45 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public int strStr(String haystack, String needle) {
 
-        Deque<TreeNode> queue = new ArrayDeque<>(); 
-        if(root == null) return new ArrayList<>();
-        queue.offer(root);
+        int m = haystack.length();
+        int n = needle.length();
 
-        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i <= m - n; i++){
+            int j;
+           
+            for( j = 0; j < n; j++){
 
-
-    while(!queue.isEmpty()){
-
-        List<Integer> currentLevel = new ArrayList<>();
-
-        int levelSize = queue.size();
-
-        for(int i = 0; i < levelSize; i++){
-
-            TreeNode node = queue.poll();
-
-            System.out.print(currentLevel.add(node.val));
-
-            if(node.left != null) queue.offer(node.left);
-            if(node.right != null) queue.offer(node.right);
-        }
-        list.add(currentLevel);
-    }
-        return list;
+                if(haystack.charAt(i + j) != needle.charAt(j)) break;
+            }
+                if(j == n) return i;
+            }
+        
+        return -1;
     }
 }
