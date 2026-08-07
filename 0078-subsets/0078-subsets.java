cc5f11ea76be9739b6227bd03766fa062e319d26
P-Pranjali <pranjali.palsodkar@gmail.com>
2026-08-07 10:26:27 +0530

    Update 0078-subsets.java

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,24 +1,50 @@
 class Solution {
+
+List<List<Integer>> result = new ArrayList<>();
+    List<Integer> current = new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-        backtrack(nums, 0, current, result);
-        return result;
+        backtrack(0, nums);
+        return result;   
     }
 
-    private void backtrack(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
-        // Base case: what condition, and what do we do here?
-        if ( index == nums.length ) {
-            result.add(new ArrayList(current));
+    private void backtrack(int index, int[] nums){
+
+        if(index == nums.length) {
+            result.add(new ArrayList<>(current));
             return;
         }
 
-        // Choice 1: include nums[index]
         current.add(nums[index]);
-        backtrack(nums, index + 1, current, result);
-        current.remove(current.size() - 1) ;  // undo
+        backtrack(index + 1, nums);
 
-        // Choice 2: exclude nums[index]
-        backtrack(nums, index + 1 , current, result);
+        current.remove(current.size() - 1);
+
+        backtrack(index + 1, nums); 
     }
 }
+
+// class Solution {
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         List<Integer> current = new ArrayList<>();
+//         backtrack(nums, 0, current, result);
+//         return result;
+//     }
+
+//     private void backtrack(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+//         // Base case: what condition, and what do we do here?
+//         if ( index == nums.length ) {
+//             result.add(new ArrayList(current));
+//             return;
+//         }
+
+//         // Choice 1: include nums[index]
+//         current.add(nums[index]);
+//         backtrack(nums, index + 1, current, result);
+//         current.remove(current.size() - 1) ;  // undo
+
+//         // Choice 2: exclude nums[index]
+//         backtrack(nums, index + 1 , current, result);
+//     }
+// }
